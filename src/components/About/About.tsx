cf6260976e6725a2524e27c8a3d263abb93c20c1
wittyCodeX <maxcore1120@gmail.com>
2024-10-02 18:20:46 +0200

    first commit

---
+++ src/components/About/About.tsx
@@ -0,0 +1,119 @@
+import { Container } from "./styles";
+import VinayakSingh from "../../assets/VinayakSingh.png";
+import wordpress from "../../assets/wordpress.svg";
+import shopify from "../../assets/shopify.svg";
+import htmlIcon from "../../assets/html-icon.svg";
+import cssIcon from "../../assets/css-icon.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import typescriptIcon from "../../assets/typescript-icon.svg";
+import vueIcon from "../../assets/vue-icon.svg";
+import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import ScrollAnimation from "react-animate-on-scroll";
+
+export function About() {
+  return (
+    <Container id="about">
+      <div className="about-text">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <h2>About me</h2>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+          <p>
+            Hi there! I'm Nikola, a website developer with a passion for
+            creating custom online experiences for my clients. My designs follow
+            a minimalist approach and focus on simplistic powerful aesthetics. I
+            have completed a wide range of projects for 5+ years. As a Software
+            Developer, I constantly provide solutions for users’ problems.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation
+          animateIn="fadeInLeft"
+          delay={0.2 * 1000}
+          style={{ marginTop: "2rem", marginBottom: "2rem" }}
+        >
+          <p>
+            I can be working on the occasional quick fix as well as more complex
+            strategic solutions. Also, Technology is extremely fast-moving, so
+            Software Engineers need to be able to learn new programming
+            languages and technologies and adapt to the continually changing
+            environment. Thus, I love learning new frameworks and design styles
+            and push myself to stay up to date on all new web technologies. What
+            I'm truly good at—is developing applications fast, in short terms,
+            according to client requirements with Pixel Perfect quality. And I
+            have strong experience in React/React Native. I consider myself
+            creative, self-confident, and hardworking.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
+          <p>
+            So if you're in need of a new website or just looking to revamp your
+            current online presence, I'd love to chat and see how I can help.
+            Let's bring your website dreams to reality together!
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
+          <h3>Here are my main skills:</h3>
+        </ScrollAnimation>
+        <div className="hard-skills">
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.1 * 1000}>
+              <img src={wordpress} alt="Wordpress" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
+              <img src={shopify} alt="shopify" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <img src={reactIcon} alt="React" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
+              <img src={typescriptIcon} alt="Typescript" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.15 * 1000}>
+              <img src={vueIcon} alt="Vue" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
+              <img src={nodeIcon} alt="Node" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
+              <img src={htmlIcon} alt="Html" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+              <img src={cssIcon} alt="Css" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={boostrapIcon} alt="bootstrap" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={jsIcon} alt="JavaScript" />
+            </ScrollAnimation>
+          </div>
+        </div>
+      </div>
+      <div className="about-image">
+        <ScrollAnimation animateIn="fadeInRight" delay={0.2 * 1000}>
+          <img src={VinayakSingh} alt="Vinayak Singh" />
+        </ScrollAnimation>
+      </div>
+    </Container>
+  );
+}
---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,31 @@
+import { Container } from "./styles";
+import emailIcon from "../../assets/email-icon.svg";
+import phoneIcon from "../../assets/phone-icon.svg";
+import { Form } from "../Form/Form";
+
+export function Contact() {
+  return (
+    <Container id="contact">
+      <header>
+        <h2>Contact</h2>
+        <p>Ready to get started on your project? </p>
+        <p>Contact me now for a Free consultation.</p>
+      </header>
+      <div className="contacts">
+        <div>
+          <a href="mailto:maxcore1120@gamil.com">
+            <img src={emailIcon} alt="Email" />
+          </a>
+          <a href="mailto:maxcore1120@gamil.com">maxcore1120@gamil.com</a>
+        </div>
+        <div>
+          <a href="tel:+919630576848">
+            <img src={phoneIcon} alt="Phone No" />
+          </a>
+          <a href="tel:+919630576848">(+91) 9630576848</a>
+        </div>
+      </div>
+      <Form></Form>
+    </Container>
+  );
+}
